f7f553855a57df39c71666c7c0b5578e6f3877c9
Yashu Chauhan <chauhanyashu1909@gmail.com>
2024-03-26 17:31:18 +0530

    Utils Updated

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 import numpy as np
 import pickle
+import dill
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.metrics import r2_score
@@ -15,7 +16,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -57,7 +58,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models, params):
 def load_object(file_path):
     try:
         with open(file_path,'rb') as file_obj:
-         return pickle.load(file_obj)
+         return dill.load(file_obj)
         
     except Exception as e:
         raise CustomException(e,sys)
