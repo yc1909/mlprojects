002208a7166f8109406668bfab58db2f90ab3644
Yashu Chauhan <chauhanyashu1909@gmail.com>
2024-03-26 13:21:13 +0530

    Button Added

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ app = application
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
 
 @app.route('/predictdata',methods = ['GET','POST'])
 def predict_datapoint():
