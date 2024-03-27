330eb5ff3bd940f592788d6f4c0984b7fcd8c4a5
Yashu Chauhan <chauhanyashu1909@gmail.com>
2024-03-27 13:41:01 +0530

    model updated

--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -58,7 +58,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models, params):
 def load_object(file_path):
     try:
         with open(file_path,'rb') as file_obj:
-         return dill.load(file_obj)
+         return pickle.load(file_obj)
         
     except Exception as e:
         raise CustomException(e,sys)
